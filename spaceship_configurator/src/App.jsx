44f03d112d24ec9115d65288e40cb6d2d4ba87a6
raghavpatel123 <gurjjarraghav123@gmail.com>
2025-05-26 06:43:04 +0530

    first commit

---
+++ spaceship_configurator/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import ShipComponent from "./Components/ShipComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <ShipComponent />
+    </div>
+  );
+}
+
+export default App;
